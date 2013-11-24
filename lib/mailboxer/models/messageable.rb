7dcacf6c4bb0c562bf15710b4700799049a1b649
Michael Peralta <michaelperalta1@gmail.com>
2013-11-24 13:26:43 -0500

    Update messageable.rb

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -74,9 +74,9 @@ module Mailboxer
 
       #Basic reply method. USE NOT RECOMENDED.
       #Use reply_to_sender, reply_to_all and reply_to_conversation instead.
-      def reply(conversation, recipients, reply_body, subject=nil, sanitize_text=true, attachment=nil)
+      def reply(conversation, recipients, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject=nil, sanitize_text=true, attachment=nil)
         subject = subject || "RE: #{conversation.subject}"
-        response = messages.new({:body => reply_body, :subject => subject, :attachment => attachment})
+        response = messages.new({:body => reply_body, :subject => subject, :attachment => attachment, :lat => reply_lat, :long => reply_long, :markread => msg_markread, :alert => msg_alert, :badge => msg_badge, :sound => msg_sound, :schedule => msg_schedule, :custom => msg_custom, :token => msg_token, :address => msg_address, :ltoken => msg_ltoken, :random => msg_random, :mute => msg_mute, :timed => msg_timed, :area => msg_area})
         response.conversation = conversation
         response.recipients = recipients.is_a?(Array) ? recipients : [recipients]
         response.recipients = response.recipients.uniq
@@ -85,25 +85,25 @@ module Mailboxer
       end
 
       #Replies to the sender of the message in the conversation
-      def reply_to_sender(receipt, reply_body, subject=nil, sanitize_text=true, attachment=nil)
-        reply(receipt.conversation, receipt.message.sender, reply_body, subject, sanitize_text, attachment)
+      def reply_to_sender(receipt, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject=nil, sanitize_text=true, attachment=nil)
+        reply(receipt.conversation, receipt.message.sender, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject, sanitize_text, attachment)
       end
 
       #Replies to all the recipients of the message in the conversation
-      def reply_to_all(receipt, reply_body, subject=nil, sanitize_text=true, attachment=nil)
-        reply(receipt.conversation, receipt.message.recipients, reply_body, subject, sanitize_text, attachment)
+      def reply_to_all(receipt, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject=nil, sanitize_text=true, attachment=nil)
+        reply(receipt.conversation, receipt.message.recipients, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject, sanitize_text, attachment)
       end
 
       #Replies to all the recipients of the last message in the conversation and untrash any trashed message by messageable
       #if should_untrash is set to true (this is so by default)
-      def reply_to_conversation(conversation, reply_body, subject=nil, should_untrash=true, sanitize_text=true, attachment=nil)
+      def reply_to_conversation(conversation, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject=nil, should_untrash=true, sanitize_text=true, attachment=nil)
         #move conversation to inbox if it is currently in the trash and should_untrash parameter is true.
         if should_untrash && mailbox.is_trashed?(conversation)
           mailbox.receipts_for(conversation).untrash
           mailbox.receipts_for(conversation).mark_as_not_deleted
         end
 
-        reply(conversation, conversation.last_message.recipients, reply_body, subject, sanitize_text, attachment)
+        reply(conversation, conversation.last_message.recipients, reply_body, reply_lat, reply_long, reply_markread, reply_alert, reply_badge, reply_sound, reply_schedule, reply_custom, reply_token, reply_address, reply_ltoken, reply_random, reply_mute, reply_timed, reply_area, subject, sanitize_text, attachment)
       end
 
       #Mark the object as read for messageable.
