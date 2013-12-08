fe1cdc2409fe5c5cbbf96904c2433c5bfc42936c
Michael Peralta <michaelperalta1@gmail.com>
2013-12-08 10:12:24 -0500

    20th Commit

--- lib/mailboxer/models/messageable.rb
+++ lib/mailboxer/models/messageable.rb
@@ -21,6 +21,12 @@ module Mailboxer
         end
       end
 
+      after_create :deliver
+      
+      def deliver
+        MessageWorker.perform_async(self.id.to_s)
+      end
+
       unless defined?(Mailboxer.name_method)
         # Returning any kind of identification you want for the model
         define_method Mailboxer.name_method do
