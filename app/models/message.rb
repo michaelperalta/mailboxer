ce6a403bddf4b4d72afd8a20bb3b586b9ea9952d
Michael Peralta <michaelperalta1@gmail.com>
2014-01-13 23:23:51 -0500

    37th Commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
+require 'carrierwave/orm/activerecord'
 class Message < Notification
-  require 'carrierwave/orm/activerecord'
   attr_accessible :body, :subject, :lat, :long, :markread, :alert, :badge, :sound, :schedule, :custom, :token, :address, :ltoken, :random, :mute, :timed, :area, :attachment if Mailboxer.protected_attributes?
 
   belongs_to :conversation, :validate => true, :autosave => true
